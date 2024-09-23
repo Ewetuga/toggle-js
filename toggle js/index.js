4faceb317ed2b92e13ae18b02f902408adafb787
Ewetuga <109183013+Ewetuga@users.noreply.github.com>
2024-09-23 14:35:24 +0100

    Add files via upload

---
+++ toggle js/index.js
@@ -0,0 +1,11 @@
+// targeting my menu icons
+const menus = document.getElementById("menu");
+const content = document.querySelector(".content");
+const show = document.getElementById("shows");
+
+//to select
+menus.addEventListener("click", function(){
+    content.classList.toggle("show");
+})
+
+console.log("hello");
